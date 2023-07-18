3cdebdadec5224cd43a89e2b3f67b3b7a8883af9
sbendarsky <sbendarsky@gmail.com>
2023-07-18 19:47:23 +0300

    added function that reloads the page every 5 seconds to get real time data. updated index.html & app.py, and updated requirements.txt

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import psutil
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, jsonify
 
 app = Flask(__name__, static_folder='images')
 
@@ -10,14 +10,21 @@ def serve_static():
     return send_from_directory('images', filename)
 
 
+@app.route("/data")
+def data():
+    cpu_usage = psutil.cpu_percent()
+    mem_usage = psutil.virtual_memory().percent
+    msg = "OK"
+    if cpu_usage > 20 or mem_usage > 80:
+        msg = "Warning"
+    return jsonify(cpu_usage=cpu_usage, mem_usage=mem_usage, msg=msg)
+
+
 @app.route("/")
 def index():
     cpu_usage = psutil.cpu_percent()
     mem_usage = psutil.virtual_memory().percent
-    msg = "OK"
-    if cpu_usage > 80 or mem_usage > 80:
-        msg = "Warning"
-    return render_template("index.html", cpu_usage=cpu_usage, mem_usage=mem_usage, msg=msg)
+    return render_template("index.html", cpu_usage=cpu_usage, mem_usage=mem_usage)
 
 
 if __name__ == "__main__":
