d4a5466e2170fb1e2ee892bdd7fb7d4ce6137bbe
sbendarsky <sbendarsky@gmail.com>
2023-07-18 18:27:49 +0300

    update: - added background locally. - updated index.html & app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import psutil
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='images')
+
+
+@app.route('/images/background.jpeg')
+def serve_static():
+    filename = 'background.jpeg'
+    return send_from_directory('images', filename)
 
 
 @app.route("/")
@@ -11,7 +17,7 @@ def index():
     msg = "OK"
     if cpu_usage > 80 or mem_usage > 80:
         msg = "Warning"
-    return render_template("index.html", cpu_usage=cpu_usage, mem_usage=mem_usage, msg=msg)
+    return render_template("index.html", cpu_usage=cpu_usage, mem_usage=mem_usage, msg=msg, background=send_from_directory('images', 'background.jpeg'))
 
 
 if __name__ == "__main__":
